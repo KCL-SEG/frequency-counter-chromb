3d7a4f1c22433a13ef6352204a7138b59ff965a0
chromb <ge11684@outlook.com>
2023-09-27 19:08:50 +0100

    Add Comments

--- frequencies.py
+++ frequencies.py
@@ -3,11 +3,11 @@
 
 def frequencies(items):
     frequencies = {}
-    for item in items:
-        if type(item) != str:
-            item = str(item)
+    for item in items: # for each item in the list
+        if type(item) != str: #check if it is a string
+            item = str(item) #if not, cast to a string
         if item in frequencies:
-            frequencies[item] += 1
+            frequencies[item] += 1 #if item is already in the dictionary, add to it's counter
         else:
-            frequencies[item] = 1
+            frequencies[item] = 1 #else add it to the dictionary
     return frequencies
